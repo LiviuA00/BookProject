4173ce9761eb436ce4bccd6c73ec3d52d03a1546
LiviuA00 <liviuanton418@yahoo.com>
2023-12-13 19:34:39 +0200

    sign up and login

--- src/config.js
+++ src/config.js
@@ -1,2 +1,26 @@
 const mongoose = require("mongoose");
-const connect = mongoose.connect("")
+const connect = mongoose.connect("mongodb://0.0.0.0/BookProject");
+
+connect.then(() => {
+    console.log("Database connected Successfully");
+})
+.catch((error) => {
+    console.error("Database cannot be connected:", error);
+});
+
+
+const LoginSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+//model
+const collection = new mongoose.model("users", LoginSchema);
+
+module.exports = collection;
